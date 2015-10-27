c67655a119788dedf5d28eb2e42a60ea015018be
AlexSoft <fantom1432@gmail.com>
2015-10-28 00:58:15 +0500

    Create generate.php

---
+++ generate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Александр
+ * Date: 26.10.15
+ * Time: 17:18
+ * Файл генерации каптчи - модель для генерации в класс Captcha.class.php который будет в dir class/Security/
+ */
+session_start();
+$_SESSION['source'] = rand(1000,9999);
+
+include_once 'class/Captcha.class.php';
+
+$captcha = new Captcha(); //Инициализация класса капчи
+
+echo $captcha->generate(); //запуск итогового изображения
